a124992594e5ddd05d1e551118584dc48e28d63e
Diab <P10361344@capita.co.uk>
2018-01-23 12:31:43 +0000

    Init web server

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "linebreak-style": 0,
+        "no-console": 0,
+        "no-var":0,
+        "func-names": 0, 
+        "prefer-arrow-callback": 0, 
+        "no-unused-vars":1,
+    }
+};
--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello world')
+const http = require('http');
+
+var server = http.createServer(function (req, res) {
+  console.log('Incoming request: ', req.url);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello world');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Now listening on 127.0.0.1:3000');
