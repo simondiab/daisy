c37bf5deaab69d9d0355955b169e482d4e386c8b
Diab <P10361344@capita.co.uk>
2018-01-23 17:10:06 +0000

    Feck eslint entirely

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const http = require('http');
-const fs = require('fs');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 var server = http.createServer(function (req, res) {
   console.log('Incoming request: ', req.url);
   if (req.url === '/home' || req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    var myReadStream = fs.createReadStream(path.join(__dirname, 'index.html'));
     myReadStream.pipe(res);
   } else if (req.url === '/api') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -18,6 +19,7 @@ var server = http.createServer(function (req, res) {
     res.end(JSON.stringify(jsonObj));
   }
 });
-
 server.listen(3000, '127.0.0.1');
 console.log('Now listening on 127.0.0.1:3000');
+
+
