f5df35a2d9d081bd7c9ef027e08fb9f9c9185189
Diab <P10361344@capita.co.uk>
2018-01-23 17:22:57 +0000

    Add 404 and express module

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ var fs = require('fs');
 var path = require('path');
 
 var server = http.createServer(function (req, res) {
-  console.log('Incoming request: ', req.url);
-  if (req.url === '/home' || req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    var myReadStream = fs.createReadStream(path.join(__dirname, 'index.html'));
-    myReadStream.pipe(res);
-  } else if (req.url === '/api') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    var jsonObj = {
-      quote: 'Annual income twenty pounds, annual expenditure nineteen pounds nineteen and six, result happiness. Annual income twenty pounds, annual expenditure twenty pounds nought and six, result misery.',
-      character: 'Wilkins Micawber',
-      work: 'David Copperfield',
-      creator: 'Charles Dickens',
-    };
-    res.end(JSON.stringify(jsonObj));
-  }
+    console.log('Incoming request: ', req.url);
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        var myReadStream = fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
+    } else if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var jsonObj = {
+            quote: 'Annual income twenty pounds, annual expenditure nineteen pounds nineteen and six, result happiness. Annual income twenty pounds, annual expenditure twenty pounds nought and six, result misery.',
+            character: 'Wilkins Micawber',
+            work: 'David Copperfield',
+            creator: 'Charles Dickens',
+        };
+        res.end(JSON.stringify(jsonObj));
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        var myReadStream = fs.createReadStream(path.join(__dirname, '404.html')).pipe(res);
+    }
 });
 server.listen(3000, '127.0.0.1');
 console.log('Now listening on 127.0.0.1:3000');
