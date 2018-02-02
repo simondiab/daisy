b445910a5ce1d372d4e656acb24a3eefc1d40b1f
Diab <P10361344@capita.co.uk>
2018-02-02 17:01:13 +0000

    Friday cleanup

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 var path = require('path');
+var bodyParser = require('body-parser');
 var express = require('express');
-var app = express();
 
+var app = express();
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: false
+});
 app.set('view engine', 'ejs');
 
 app.use('/assets', express.static('assets'));
 
+// app.use(function (req,res,next) {
+//     res.status('404').render('404');
+// });
+
 app.get('/', function (req, res) {
-   res.render('index');
+    res.render('index');
 });
 
 app.get('/contact', function (req, res) {
-   res.render('contact');
+    console.log(req.query);
+    res.render('contact', {
+        qs: req.query
+    });
 });
 
-app.get('/profile/:name', function(req, res) {
+app.post('/contact', urlEncodedParser, function (req, res) {
+    console.log(req.body);
+    res.render('contact', {
+        qs: req.query
+    });
+});
+
+app.get('/profile/:name', function (req, res) {
     var data = {
         age: 29,
         job: 'teacher',
@@ -26,7 +44,7 @@ app.get('/profile/:name', function(req, res) {
     });
 });
 
-app.get('/league/:team/:player', function(req, res) {
+app.get('/league/:team/:player', function (req, res) {
     res.send('You asked to see the player profile for ' + req.params.player + ' who plays for ' + req.params.team );
 });
 
